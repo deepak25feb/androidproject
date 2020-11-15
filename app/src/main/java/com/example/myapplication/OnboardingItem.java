01c6900cee02fed8fc313fb4add765dce72f2469
DPKKMLJIYA <singh.deepak.18bit056@gmail.com>
2020-11-15 18:39:46 +0530

    ViewPager2

---
+++ app/src/main/java/com/example/myapplication/OnboardingItem.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+public class OnboardingItem {
+    private int image;
+
+    public OnboardingItem(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
